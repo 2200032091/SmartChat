80323d9928296eb81467810f2c7c036e604cb47e
Harika <gharika109@gmail.com>
2024-11-13 22:47:42 +0530

    push project

---
+++ SmartChat/frontend/src/components/typer/TypingAnim.tsx
@@ -0,0 +1,25 @@
+import { TypeAnimation } from "react-type-animation";
+
+const TypingAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed once, initially
+        "Chat With Your OWN AI",
+        500,
+        "Your Own Customized ChatGPT",
+        500,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        textShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TypingAnim;
